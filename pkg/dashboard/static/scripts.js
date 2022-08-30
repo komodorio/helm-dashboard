98448d670ad86e88b23927ae42b5da993c9a45e4
Andrei Pohilko <andrei.pokhilko@gmail.com>
2022-08-30 11:50:58 +0100

    Sticky URL

--- pkg/dashboard/static/scripts.js
+++ pkg/dashboard/static/scripts.js
@@ -5,27 +5,42 @@ function reportError(err) {
     alert(err) // TODO: nice modal/baloon/etc
 }
 
-$(function () {
-    // cluster list
-    $.getJSON("/api/kube/contexts").fail(function () {
+function fillChartDetails(namespace, name) {
+    $("#sectionDetails").show()
+    $("#sectionDetails h1 span").text(name)
+    $.getJSON("/api/helm/charts/history?chart=" + name + "&namespace=" + namespace).fail(function () {
         reportError("Failed to get list of clusters")
     }).done(function (data) {
         data.forEach(function (elm) {
-            // aws CLI uses complicated context names, the suffix does not work well
-            // maybe we should have an `if` statement here
-            let label = elm.Name //+ " (" + elm.Cluster + "/" + elm.AuthInfo + "/" + elm.Namespace + ")"
-            let opt = $("<option></option>").val(elm.Name).text(label)
-            if (elm.IsCurrent) {
-                opt.attr("selected", "selected")
+            const rev = $(`            
+                            <div class="col-md-2 rounded border border-secondary bg-gradient bg-white">
+                                <span><b class="rev-number"></b> - <span class="rev-status"></span></span><br/>
+                                <span class="text-muted">Chart:</span> <span class="chart-ver"></span><br/>
+                                <span class="text-muted">App:</span> <span class="app-ver"></span><br/>
+                                <span class="text-muted small rev-date"></span><br/>                
+                            </div>`)
+            rev.find(".rev-number").text("#" + elm.revision)
+            rev.find(".app-ver").text(elm.app_version)
+            rev.find(".chart-ver").text(elm.chart_ver)
+            rev.find(".rev-date").text(elm.updated.replace("T", " "))
+            rev.find(".rev-status").text(elm.status).attr("title", elm.action)
+
+            if (elm.status === "failed") {
+                rev.find(".rev-status").parent().addClass("text-danger")
             }
-            clusterSelect.append(opt)
+
+            if (elm.status === "deployed") {
+                rev.removeClass("bg-white").addClass("text-light bg-primary")
+            }
+
+            $("#sectionDetails .row").append(rev)
         })
     })
-    clusterSelect.change(function () {
-        // TODO: remember it, respect it in the function above and in all other places
-    })
 
-    // charts list
+}
+
+function loadChartsList() {
+    $("#sectionList").show()
     $.getJSON("/api/helm/charts").fail(function () {
         reportError("Failed to get list of clusters")
     }).done(function (data) {
@@ -56,41 +71,41 @@ $(function () {
             card.click(function () {
                 const self = $(this)
                 $("#sectionList").hide()
-                $("#sectionDetails").show()
 
                 let chart = self.data("chart");
-                $("#sectionDetails h1 span").text(chart.name)
-                $.getJSON("/api/helm/charts/history?chart=" + chart.name + "&namespace=" + chart.namespace).fail(function () {
-                    reportError("Failed to get list of clusters")
-                }).done(function (data) {
-                    data.forEach(function (elm) {
-                        const rev = $(`            
-                            <div class="col-md-2 rounded border border-secondary bg-gradient bg-white">
-                                <span><b class="rev-number"></b> - <span class="rev-status"></span></span><br/>
-                                <span class="text-muted">Chart:</span> <span class="chart-ver"></span><br/>
-                                <span class="text-muted">App:</span> <span class="app-ver"></span><br/>
-                                <span class="text-muted small rev-date"></span><br/>                
-                            </div>`)
-                        rev.find(".rev-number").text("#" + elm.revision)
-                        rev.find(".app-ver").text(elm.app_version)
-                        rev.find(".chart-ver").text(elm.chart_ver)
-                        rev.find(".rev-date").text(elm.updated.replace("T", " "))
-                        rev.find(".rev-status").text(elm.status).attr("title", elm.action)
-
-                        if (elm.status === "failed") {
-                            rev.find(".rev-status").parent().addClass("text-danger")
-                        }
-
-                        if (elm.status === "deployed") {
-                            rev.removeClass("bg-white").addClass("text-light bg-primary")
-                        }
-
-                        $("#sectionDetails .row").append(rev)
-                    })
-                })
+                window.location.hash = chart.namespace + "&" + chart.name
+                fillChartDetails(chart.namespace, chart.name)
             })
 
             chartsCards.append($("<div class='col'></div>").append(card))
         })
     })
+}
+
+$(function () {
+    // cluster list
+    $.getJSON("/api/kube/contexts").fail(function () {
+        reportError("Failed to get list of clusters")
+    }).done(function (data) {
+        data.forEach(function (elm) {
+            // aws CLI uses complicated context names, the suffix does not work well
+            // maybe we should have an `if` statement here
+            let label = elm.Name //+ " (" + elm.Cluster + "/" + elm.AuthInfo + "/" + elm.Namespace + ")"
+            let opt = $("<option></option>").val(elm.Name).text(label)
+            if (elm.IsCurrent) {
+                opt.attr("selected", "selected")
+            }
+            clusterSelect.append(opt)
+        })
+    })
+    clusterSelect.change(function () {
+        // TODO: remember it, respect it in the function above and in all other places
+    })
+
+    const parts = window.location.hash.split("&")
+    if (parts[0] === "") {
+        loadChartsList()
+    } else {
+        fillChartDetails(parts[0], parts[1])
+    }
 })
