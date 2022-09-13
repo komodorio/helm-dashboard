c1b87dce78ad8b877a9d5ebeb03762d5fe31066c
Andrei Pohilko <andrei.pokhilko@gmail.com>
2022-09-13 22:37:46 +0100

    refactor out kube handlers

--- pkg/dashboard/api.go
+++ pkg/dashboard/api.go
@@ -5,8 +5,6 @@ import (
 	"errors"
 	"github.com/gin-gonic/gin"
 	log "github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/apis/meta/v1"
-	v12 "k8s.io/apimachinery/pkg/apis/testapigroup/v1"
 	"net/http"
 	"os"
 	"path"
@@ -80,62 +78,10 @@ func configureHelms(api *gin.RouterGroup, data *DataLayer) {
 }
 
 func configureKubectls(api *gin.RouterGroup, data *DataLayer) {
-	api.GET("/contexts", func(c *gin.Context) {
-		res, err := data.ListContexts()
-		if err != nil {
-			_ = c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-		c.IndentedJSON(http.StatusOK, res)
-	})
-
-	api.GET("/resources/:kind", func(c *gin.Context) {
-		qp, err := getQueryProps(c, false)
-		if err != nil {
-			_ = c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		res, err := data.GetResource(qp.Namespace, &GenericResource{
-			TypeMeta:   v1.TypeMeta{Kind: c.Param("kind")},
-			ObjectMeta: v1.ObjectMeta{Name: qp.Name},
-		})
-		if err != nil {
-			_ = c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		if res.Status.Phase == "Active" || res.Status.Phase == "Error" {
-			_ = res.Name + ""
-		} else if res.Status.Phase == "" && len(res.Status.Conditions) > 0 {
-			res.Status.Phase = v12.CarpPhase(res.Status.Conditions[len(res.Status.Conditions)-1].Type)
-			res.Status.Message = res.Status.Conditions[len(res.Status.Conditions)-1].Message
-			res.Status.Reason = res.Status.Conditions[len(res.Status.Conditions)-1].Reason
-			if res.Status.Conditions[len(res.Status.Conditions)-1].Status == "False" {
-				res.Status.Phase = "Not" + res.Status.Phase
-			}
-		} else if res.Status.Phase == "" {
-			res.Status.Phase = "Exists"
-		}
-
-		c.IndentedJSON(http.StatusOK, res)
-	})
-
-	api.GET("/describe/:kind", func(c *gin.Context) {
-		qp, err := getQueryProps(c, false)
-		if err != nil {
-			_ = c.AbortWithError(http.StatusBadRequest, err)
-			return
-		}
-
-		res, err := data.DescribeResource(qp.Namespace, c.Param("kind"), qp.Name)
-		if err != nil {
-			_ = c.AbortWithError(http.StatusInternalServerError, err)
-			return
-		}
-
-		c.String(http.StatusOK, res)
-	})
+	h := KubeHandler{Data: data}
+	api.GET("/contexts", h.GetContexts)
+	api.GET("/resources/:kind", h.GetResourceInfo)
+	api.GET("/describe/:kind", h.Describe)
 }
 
 func configureStatic(api *gin.Engine) {
