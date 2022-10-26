7d50f4e6200ad97628671525d442d54f088e606b
Andrei Pohilko <andrei.pokhilko@gmail.com>
2022-10-26 16:14:10 +0100

    Recognize "Bound" status as normal

--- pkg/dashboard/static/details-view.js
+++ pkg/dashboard/static/details-view.js
@@ -172,7 +172,7 @@ function showResources(namespace, chart, revision) {
                 //reportError("Failed to get list of resources")
             }).done(function (data) {
                 const badge = $("<span class='badge me-2 fw-normal'></span>").text(data.status.phase);
-                if (["Available", "Active", "Established"].includes(data.status.phase)) {
+                if (["Available", "Active", "Established", "Bound"].includes(data.status.phase)) {
                     badge.addClass("bg-success text-dark")
                 } else if (["Exists"].includes(data.status.phase)) {
                     badge.addClass("bg-success text-dark bg-opacity-50")
