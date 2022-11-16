0bddbcb33642cfdb22b4fa5693d84e8df0747fa7
ronahk <rona@komodor.io>
2022-11-16 13:20:21 +0200

    remove commented line

--- pkg/dashboard/static/scripts.js
+++ pkg/dashboard/static/scripts.js
@@ -7,7 +7,6 @@ $(function () {
     $.getJSON("/api/kube/contexts").fail(function (xhr) {
         reportError("Failed to get list of clusters", xhr)
     }).done(function (data) {
-        //$("body")[0].contexts = data
         $("body").data("contexts", data)
         const context = getHashParam("context")
         data.sort((a, b) => (getCleanClusterName(a.Name) > getCleanClusterName(b.Name)) - (getCleanClusterName(a.Name) < getCleanClusterName(b.Name)))
