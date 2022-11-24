ec86ed0913ae96446e0e396d7ee15829c741110b
Itiel Shwartz <itielshw@gmail.com>
2022-11-24 12:21:57 +0200

    revert main changes

--- main.go
+++ main.go
@@ -11,16 +11,20 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+var (
+	version = "0.0.0"
+	commit  = "none"
+	date    = "unknown"
+)
+
 type options struct {
-	Version    string `long:"version" description:"Show tool version"  default:"0.0.0"`
+	Version    bool   `long:"version" description:"Show tool version"`
 	Verbose    bool   `short:"v" long:"verbose" description:"Show verbose debug information"`
 	NoBrowser  bool   `short:"b" long:"no-browser" description:"Do not attempt to open Web browser upon start"`
 	NoTracking bool   `long:"no-analytics" description:"Disable user analytics (GA, DataDog etc.)"`
 	BindHost   string `long:"bind" description:"Host binding to start server (default: localhost)"` // default should be printed but not assigned as the precedence: flag > env > default
 	Port       uint   `short:"p" long:"port" description:"Port to start server on" default:"8080"`  // TODO: better default port to clash less?
 	Namespace  string `short:"n" long:"namespace" description:"Limit operations to a specific namespace"`
-	Commit     string `long:"commit" description:"Build commit" default:"none"`
-	Date       string `long:"date" description:"build date" default:"unknown"`
 }
 
 func main() {
@@ -33,9 +37,10 @@ func main() {
 		opts.BindHost = host
 	}
 
-	setupLogging(opts.Verbose, opts.Version, opts.Commit, opts.Date)
+	setupLogging(opts.Verbose)
+
 	server := dashboard.Server{
-		Version:    opts.Version,
+		Version:    version,
 		Namespace:  opts.Namespace,
 		Address:    fmt.Sprintf("%s:%d", opts.BindHost, opts.Port),
 		Debug:      opts.Verbose,
@@ -80,13 +85,18 @@ func parseFlags() options {
 		os.Exit(1)
 	}
 
+	if opts.Version {
+		fmt.Print(version)
+		os.Exit(0)
+	}
+
 	if len(args) > 0 {
 		panic("The program does not take argumants, see --help for usage")
 	}
 	return opts
 }
 
-func setupLogging(verbose bool, version string, commit string, date string) {
+func setupLogging(verbose bool) {
 	if verbose {
 		log.SetLevel(log.DebugLevel)
 		gin.SetMode(gin.DebugMode)
