20f23b3863f88e9ee8a7d00bd54073cda72cf1cb
OmAxiani0 <aximaniom@gmail.com>
2022-12-26 15:43:32 +0530

    added changes

--- pkg/dashboard/api_test.go
+++ pkg/dashboard/api_test.go
@@ -20,9 +20,23 @@ func GetTestGinContext(w *httptest.ResponseRecorder) *gin.Context {
 	return ctx
 }
 
-func TestNoCache(t *testing.T) {
+func TestNoCacheBeforeSetting(t *testing.T) {
 	w := httptest.NewRecorder()
 	con := GetTestGinContext(w)
+	noCache(con)
+	assert.Equal(t, w.Header().Get("Cache-Control"), "no-cache")
+}
+
+func TestNoCacheAfterSetting(t *testing.T) {
+	w := httptest.NewRecorder()
+	con := GetTestGinContext(w)
+
+	// Set value to default value defined in `EnableClientCache` (reference to below file)
+	// https://github.com/komodorio/helm-dashboard/blob/features-1.0/pkg/dashboard/handlers/common.go
+	w.Header().Set("Cache-Control", "max-age=43200")
+	assert.Equal(t, w.Header().Get("Cache-Control"), "max-age=43200")
+
+	// Now manipulate the value with `noCache()`
 	noCache(con)
 	assert.Equal(t, w.Header().Get("Cache-Control"), "no-cache")
 }
