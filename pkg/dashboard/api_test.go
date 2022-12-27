f8f56494ffb34125fa4f2e879cb4689926bf2166
Om Aximani <75031769+OmAximani0@users.noreply.github.com>
2022-12-27 18:11:53 +0530

    test for static route (#153)

--- pkg/dashboard/api_test.go
+++ pkg/dashboard/api_test.go
@@ -44,3 +44,23 @@ func TestEnableCacheControl(t *testing.T) {
 	h.EnableClientCache(con)
 	assert.Equal(t, w.Header().Get("Cache-Control"), "max-age=43200")
 }
+
+func TestConfigureStatic(t *testing.T) {
+	w := httptest.NewRecorder()
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a API Engine
+	api := gin.Default()
+
+	// Configure static routes
+	configureStatic(api)
+
+	// Start the server
+	api.ServeHTTP(w, req)
+
+	assert.Equal(t, w.Code, http.StatusOK)
+}
