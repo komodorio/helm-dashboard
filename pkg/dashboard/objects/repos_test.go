9451a899d714ffecd7435318440a573421ce7c24
Om Aximani <75031769+OmAximani0@users.noreply.github.com>
2023-01-06 00:26:04 +0530

    Fixes failing test (#171)
    * Fixes failing test
    - Fixes failing test of repo loading
    
    * handles error for
    
    * Did some changes

--- pkg/dashboard/objects/repos_test.go
+++ pkg/dashboard/objects/repos_test.go
@@ -1,31 +1,27 @@
 package objects
 
 import (
-	"os"
 	"testing"
 
 	"gotest.tools/v3/assert"
 	"helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/helmpath"
 	"helm.sh/helm/v3/pkg/repo"
 )
 
-func envOr(name, def string) string {
-	if v, ok := os.LookupEnv(name); ok {
-		return v
-	}
-	return def
-}
-
 func TestLoadRepo(t *testing.T) {
 
-	filePath := envOr("HELM_REPOSITORY_CONFIG", helmpath.ConfigPath("repositories.yaml"))
+	filePath := "./testdata/repositories.yaml"
+
 	res, err := repo.LoadFile(filePath)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	settings := cli.New()
+
+	// Sets the repository file path
+	settings.RepositoryConfig = filePath
+
 	hc, err := NewHelmConfig(settings, "TEST")
 	if err != nil {
 		t.Fatal(err, "Failed to create Helm Configuration")
