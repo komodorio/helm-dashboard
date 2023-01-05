14faed655673758650d62849582e4bfb184cb4b5
OmAxiani0 <aximaniom@gmail.com>
2023-01-05 22:06:46 +0530

    Did some changes

--- pkg/dashboard/objects/repos_test.go
+++ pkg/dashboard/objects/repos_test.go
@@ -1,49 +1,27 @@
 package objects
 
 import (
-	"io/ioutil"
-	"os"
-	"path"
 	"testing"
 
-	"gopkg.in/yaml.v3"
 	"gotest.tools/v3/assert"
 	"helm.sh/helm/v3/pkg/cli"
-	"helm.sh/helm/v3/pkg/helmpath"
 	"helm.sh/helm/v3/pkg/repo"
 )
 
-func envOr(name, def string) string {
-	if v, ok := os.LookupEnv(name); ok {
-		return v
-	}
-	return def
-}
-
 func TestLoadRepo(t *testing.T) {
 
-	fileName := "repositories.yaml"
-	tmpDir := t.TempDir()
+	filePath := "./testdata/repositories.yaml"
 
-	os.Setenv("HELM_CONFIG_HOME", tmpDir)
-
-	yamlData, err := yaml.Marshal(repo.NewFile())
-	if err != nil {
-		t.Fatal(err, "failed to create YAML bytes")
-	}
-
-	err = ioutil.WriteFile(path.Join(tmpDir, fileName), yamlData, 0644)
-	if err != nil {
-		t.Fatal(err, "failed to create a test file")
-	}
-
-	filePath := envOr("HELM_REPOSITORY_CONFIG", helmpath.ConfigPath(fileName))
 	res, err := repo.LoadFile(filePath)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	settings := cli.New()
+
+	// Sets the repository file path
+	settings.RepositoryConfig = filePath
+
 	hc, err := NewHelmConfig(settings, "TEST")
 	if err != nil {
 		t.Fatal(err, "Failed to create Helm Configuration")
