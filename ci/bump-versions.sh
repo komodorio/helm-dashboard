6ffcdf2b8e44309cc69ecbe22a9747ad93583514
Andrei Pohilko <andrei.pokhilko@gmail.com>
2023-02-01 09:58:26 +0000

    Commit plugin.yaml update upon release

--- ci/bump-versions.sh
+++ ci/bump-versions.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 
 WORKING_DIRECTORY="$PWD"
 
@@ -9,6 +9,7 @@ WORKING_DIRECTORY="$PWD"
 }
 
 sed -i -e "s/appVersion.*/appVersion: \"${APP_VERSION}\" /g" ${HELM_CHARTS_SOURCE}/Chart.yaml
+sed -i -e "s/version.*/version: \"${APP_VERSION}\" /g" plugin.yaml
 CURRENT_VERSION=$(cat ${HELM_CHARTS_SOURCE}/Chart.yaml | grep 'version:' | awk '{print $2}')
 NEW_VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 sed -i -e "s/${CURRENT_VERSION}/${NEW_VERSION}/g" ${HELM_CHARTS_SOURCE}/Chart.yaml
