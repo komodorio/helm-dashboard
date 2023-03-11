5e59756f3c95a741c5fc82dba4c6fb9251d348bf
chad11111 <127234894+chad11111@users.noreply.github.com>
2023-03-11 11:28:48 -0500

    vite init (#240)

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,26 +1,13 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
+import './App.css'
+import Badge from './components/Badge'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Badge type="error"> Hello </Badge>
+      <Badge type="success"> Hello </Badge>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
