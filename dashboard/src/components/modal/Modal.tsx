acfa6077d3a69dd008abd111013f68f76d94feca
Erez Birenholz <biran330@gmail.com>
2023-03-14 05:28:00 +0200

    Make the title optional

--- dashboard/src/components/modal/Modal.tsx
+++ dashboard/src/components/modal/Modal.tsx
@@ -12,7 +12,7 @@ export type ModalAction = {
 };
 
 type ModalProps = {
-  title: string | ReactNode;
+  title?: string | ReactNode;
   isOpen: boolean;
   onClose: () => void;
   actions?: ModalAction[];
@@ -38,30 +38,32 @@ const Modal = ({ title, isOpen, onClose, children, actions }: ModalProps) => {
 
   return isVisible ? (
     <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-      <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
-          {title}
-        </h3>
-        <button
-          type="button"
-          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-          data-modal-hide="staticModal"
-          onClick={onClose}
-        >
-          <svg
-            className="w-5 h-5"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
+      {title && (
+        <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+          <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+            {title}
+          </h3>
+          <button
+            type="button"
+            className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-hide="staticModal"
+            onClick={onClose}
           >
-            <path
-              fill-rule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clip-rule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </div>
+            <svg
+              className="w-5 h-5"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clip-rule="evenodd"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      )}
       <div className="p-6 space-y-6">{children}</div>
       <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
         {actions?.map((action) => (
