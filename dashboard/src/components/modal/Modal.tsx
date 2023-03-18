51a773a7eb378190c742902ca21c04fb4549982d
Erez Birenholz <biran330@gmail.com>
2023-03-18 22:21:57 +0200

    Create an empty global modal (#253)
    * Increment chart versions [skip ci]
    
    * Minor UI fixes (#234)
    
    * Add username and password support to Repo add feature (#228)
    
    * Add username and password support to Repo add in UI
    
    * Add support for Username and Passowrd in Add Repo API
    
    * Display resource health aggregate icons on list of releases (#235)
    
    * Display aggregate resource health status
    
    * Reuse old API request, show icons
    
    * Take progress indication from deployment conditions
    
    * Improve status
    
    * Cleanup
    
    * Fixups
    
    * Squares approach
    
    * Improve console message in case no k8s connection possible
    
    * Count API docs in user analytics
    
    * Create first Generic Modal + add storybook script
    
    * change default modal button style
    
    * Create anum for button styles
    
    * fix type in console log
    
    * Make the title completely customizable
    
    * change story name
    
    * Make the title optional
    
    * implement auto scroll when height bigger than 500
    
    * fix story example
    
    * Change buttons in modal stories
    
    * fix type
    
    * Revert "Merge branch 'komodorio:main' into create-an-empty-global-modal"
    
    This reverts commit e6aae0bc61a7147ef0d72d07ad47ff8bd6b3c12d, reversing
    changes made to e37c6fe38e2113fd3192f232e0f39e63ceaa190d.
    
    * Add className prop for fully customization
    
    * Change buttonStyle to variant
    
    * add id to ModalAction
    
    * Add modal to portal div
    
    * Add background with opacity
    
    * change from type to interface
    
    * fix modal margin
    
    * Add it for the modal storybook
    
    ---------
    
    Co-authored-by: komodor-bot <komi@komodor.io>
    Co-authored-by: Harshit Mehta <hdm23061993@gmail.com>
    Co-authored-by: Andrey Pokhilko <andrei.pokhilko@gmail.com>

---
+++ dashboard/src/components/modal/Modal.tsx
@@ -0,0 +1,126 @@
+import { useState, useEffect, PropsWithChildren, ReactNode } from "react";
+import ReactDom from "react-dom";
+
+export enum ModalButtonStyle {
+  default,
+  info,
+  error,
+  success,
+}
+
+export interface ModalAction {
+  id: string,
+  callback: () => void,
+  text: string,
+  variant?: ModalButtonStyle,
+  className?: string
+}
+
+export interface ModalProps extends PropsWithChildren {
+  title?: string | ReactNode,
+  isOpen: boolean,
+  onClose: () => void,
+  actions?: ModalAction[],
+}
+
+const Modal = ({ title, isOpen, onClose, children, actions }: ModalProps) => {
+  const [isVisible, setIsVisible] = useState<boolean>(isOpen);
+
+  const colorVariants = new Map<ModalButtonStyle, string>([
+    [
+      ModalButtonStyle.default,
+      "text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600",
+    ],
+    [
+      ModalButtonStyle.info,
+      "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800",
+    ],
+    [
+      ModalButtonStyle.success,
+      "text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800",
+    ],
+    [
+      ModalButtonStyle.error,
+      "text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800",
+    ],
+  ]);
+
+  useEffect(() => {
+    setIsVisible(isOpen);
+  }, [isOpen]);
+
+  const getClassName = (action: ModalAction) => {
+    if (action.className) return action.className;
+
+    return action.variant
+      ? colorVariants.get(action.variant)
+      : colorVariants.get(ModalButtonStyle.default);
+  };
+
+  const getTitle = (title: string | ReactNode) => {
+    if (typeof title === "string")
+      return (
+        <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+          {title}
+        </h3>
+      );
+    else return title;
+  };
+
+  return ReactDom.createPortal(
+    <>
+      {isVisible && (
+        <>
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 m-7">
+            {title && (
+              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                {getTitle(title)}
+                <button
+                  type="button"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  data-modal-hide="staticModal"
+                  onClick={onClose}
+                >
+                  <svg
+                    className="w-5 h-5"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+              </div>
+            )}
+            <div
+              className="p-4 space-y-6 overflow-y-auto"
+              style={{ maxHeight: "500px" }}
+            >
+              {children}
+            </div>
+            <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+              {actions?.map((action) => (
+                <button
+                  key={action.id}
+                  type="button"
+                  className={getClassName(action)}
+                  onClick={action.callback}
+                >
+                  {action.text}
+                </button>
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </>,
+    document.getElementById("portal")
+  );
+};
+
+export default Modal;
