3b19d71de316956b1e4dae67c19846a89ff80958
Erez Birenholz <biran330@gmail.com>
2023-03-19 15:39:14 +0200

    Add Error modal

--- dashboard/src/components/modal/Modal.tsx
+++ dashboard/src/components/modal/Modal.tsx
@@ -9,21 +9,31 @@ export enum ModalButtonStyle {
 }
 
 export interface ModalAction {
-  id: string,
-  callback: () => void,
-  text: string,
-  variant?: ModalButtonStyle,
-  className?: string
+  id: string;
+  callback: () => void;
+  text: string;
+  variant?: ModalButtonStyle;
+  className?: string;
 }
 
 export interface ModalProps extends PropsWithChildren {
-  title?: string | ReactNode,
-  isOpen: boolean,
-  onClose: () => void,
-  actions?: ModalAction[],
+  title?: string | ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  containerClassNames?: string;
+  actions?: ModalAction[];
+  bottomContent?: ReactNode;
 }
 
-const Modal = ({ title, isOpen, onClose, children, actions }: ModalProps) => {
+const Modal = ({
+  title,
+  isOpen,
+  onClose,
+  children,
+  actions,
+  containerClassNames,
+  bottomContent,
+}: ModalProps) => {
   const [isVisible, setIsVisible] = useState<boolean>(isOpen);
 
   const colorVariants = new Map<ModalButtonStyle, string>([
@@ -70,53 +80,60 @@ const Modal = ({ title, isOpen, onClose, children, actions }: ModalProps) => {
   return ReactDom.createPortal(
     <>
       {isVisible && (
-        <>
-          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"></div>
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700 m-7">
-            {title && (
-              <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                {getTitle(title)}
-                <button
-                  type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                  data-modal-hide="staticModal"
-                  onClick={onClose}
-                >
-                  <svg
-                    className="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clipRule="evenodd"
-                    ></path>
-                  </svg>
-                </button>
-              </div>
-            )}
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ">
+          <div className="flex justify-center">
             <div
-              className="p-4 space-y-6 overflow-y-auto"
-              style={{ maxHeight: "500px" }}
+              className={`relative bg-white rounded-lg shadow dark:bg-gray-700 m-7 w-4/5 ${
+                containerClassNames ?? ""
+              }`}
             >
-              {children}
-            </div>
-            <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-              {actions?.map((action) => (
-                <button
-                  key={action.id}
-                  type="button"
-                  className={getClassName(action)}
-                  onClick={action.callback}
-                >
-                  {action.text}
-                </button>
-              ))}
+              {title && (
+                <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
+                  {getTitle(title)}
+                  <button
+                    type="button"
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                    data-modal-hide="staticModal"
+                    onClick={onClose}
+                  >
+                    <svg
+                      className="w-5 h-5"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              )}
+              <div
+                className="p-4 space-y-6 overflow-y-auto"
+                style={{ maxHeight: "500px" }}
+              >
+                {children}
+              </div>
+              {bottomContent ?? (
+                <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  {actions?.map((action) => (
+                    <button
+                      key={action.id}
+                      type="button"
+                      className={getClassName(action)}
+                      onClick={action.callback}
+                    >
+                      {action.text}
+                    </button>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
-        </>
+        </div>
       )}
     </>,
     document.getElementById("portal")
--- dashboard/tailwind.config.cjs
+++ dashboard/tailwind.config.cjs
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 // eslint-disable-next-line no-undef
 module.exports = {
-  content: [ "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "error-bg": "#F9D7DA",
+        "error-color": "#842029",
+        "link-color": "#0D6EFD",
+      },
+    },
   },
   plugins: [],
   fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-}
+    sans: ["Graphik", "sans-serif"],
+    serif: ["Merriweather", "serif"],
+  },
+};
