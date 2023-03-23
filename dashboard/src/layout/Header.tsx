1c769ba0a3c170d6b6bf0018c3a3d0b2395e37d2
Erez Birenholz <biran330@gmail.com>
2023-03-23 11:42:27 +0200

    Insert Upgrade helm

--- dashboard/src/layout/Header.tsx
+++ dashboard/src/layout/Header.tsx
@@ -1,7 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../App.css";
 import LogoHeader from "../assets/logo-header.svg";
 import DropDown from "../components/common/DropDown";
+import WatcherIcon from "../assets/k8s-watcher.svg";
 
 const lastRelease = "v1.2.0";
 
@@ -23,10 +24,24 @@ export default function Header() {
         <div className="inline-block">
           <ul className=" flex md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
-              <Link to="/">Installed</Link>
+              <NavLink
+                to="/"
+                className={({ isActive }) =>
+                  isActive ? "p-2 text-[#1347FF]  bg-[#EBEFFF]" : "p-2"
+                }
+              >
+                Installed
+              </NavLink>
             </li>
             <li>
-              <Link to="/Repository">Repository</Link>
+              <NavLink
+                to="/Repository"
+                className={({ isActive }) =>
+                  isActive ? "p-2 text-[#1347FF] bg-[#EBEFFF]" : "p-2"
+                }
+              >
+                Repository
+              </NavLink>
             </li>
             <li>
               <DropDown
@@ -49,8 +64,14 @@ export default function Header() {
         </div>
       </div>
       <div className="h-16 flex items-center gap-10 ">
-        <div>
-          Helm experience 
+        <div className="flex gap-2 border bottom-gray-200 rounded">
+          <img src={WatcherIcon} width={40} height={40} />
+          <div className="flex flex-col">
+            <a href="https://komodor.com/helm-dash/" className="text-[#0d6efd] font-bold">
+              Upgrade your HELM experience - Free
+            </a>
+            <label>Auth & RBAC, k8s events, troubleshooting and more</label>
+          </div>
         </div>
 
         <span className="w-[1px] h-3/4 bg-gray-200" />
