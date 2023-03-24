5ea440194eae233684d1dfe90ee7e239eac10796
aman1376 <96187306+aman1376@users.noreply.github.com>
2023-03-24 13:04:39 +0530

    Resolved conflicts

--- dashboard/src/layout/Sidebar.tsx
+++ dashboard/src/layout/Sidebar.tsx
@@ -2,24 +2,24 @@ import '../App.css'
 
 function Sidebar():JSX.Element {
   return (
-    <div className="card-left">
-          <h2>Repositories</h2>
-          <form>
-            <div className="options">
-              <label>
-                <input type="radio" value=""/>
-                bitnami
-              </label>
+    <div className="Card">
+        <h2>Repositories</h2>
+        <form>
+          <label className="repo_options"><input type="radio" className="radio-btn" name="select_repo" value="bitnami"/><span className="option">bitnami</span></label> 
+          <label className="repo_options"><input type="radio" className="radio-btn" name="select_repo" value="brigade"/><span className="option">brigade</span></label>
+        </form>
+        <button>
+            <div className="btn-element">
+                <div className="btn-logo">
+                    +
+                </div>
+                <div className="btn-title">
+                    Add Repository
+                </div>
             </div>
-            <div className="options">
-              <label>
-                <input type="radio" value=""/>
-                bitnami
-              </label>
-            </div>
-          </form>
-          <button className="btn">Add Repository</button>
-          <p>Some text that describes chart</p>
+        </button>
+        <p>Charts developers: you can also add local directories as chart source. Use <a href="" className="chart">--local-chart</a> CLI switch to specify it.</p>
+    
     </div>
   )
 }
