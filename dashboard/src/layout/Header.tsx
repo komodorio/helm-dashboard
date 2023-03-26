f7afb61d9f097c44a420625bdd95f94a41bc8500
Erez Birenholz <biran330@gmail.com>
2023-03-26 09:55:45 +0300

    Finish the Helm experience line

--- dashboard/src/layout/Header.tsx
+++ dashboard/src/layout/Header.tsx
@@ -1,21 +1,54 @@
 import { NavLink } from "react-router-dom";
 import LogoHeader from "../assets/logo-header.svg";
-import DropDown from "../components/common/DropDown";
+import DropDown, { DropDownItem } from "../components/common/DropDown";
 import WatcherIcon from "../assets/k8s-watcher.svg";
 import ShutDownButton from "./ShutDownButton";
+import {
+  BsSlack,
+  BsGithub,
+  BsArrowRepeat,
+  BsBraces,
+  BsBoxArrowUpRight,
+} from "react-icons/bs";
+import axios from "axios";
 
 const lastRelease = "v1.2.0";
 
 export default function Header() {
+  const openSupportChat = () => {
+    window.open("https://app.slack.com/client/T03Q4H8PCRW", "_blank");
+  };
+
+  const openProjectPage = () => {
+    window.open("https://github.com/komodorio/helm-dashboard", "_blank");
+  };
+
+  const resetCache = async () => {
+    try {
+      await axios.delete("/api/cache");
+      window.location.reload();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const openAPI = () => {
+    window.open("http://localhost:8080/static/api-docs.html", "_blank");
+  };
+
   return (
     <div className="h-16 flex items-center justify-between bg-white">
       <div className="h-16 flex items-center gap-6 ">
         <NavLink to="/">
-          <img src={LogoHeader} alt="Helm-DashBoard" className="ml-3 w-[140px] "/>
+          <img
+            src={LogoHeader}
+            alt="Helm-DashBoard"
+            className="ml-3 w-[140px] "
+          />
         </NavLink>
         <span className="w-[1px] h-3/4 bg-gray-200" />
         <div className="inline-block">
-          <ul className=" flex md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className=" flex md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-normal md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             <li>
               <NavLink
                 to="/"
@@ -39,9 +72,33 @@ export default function Header() {
             <li>
               <DropDown
                 items={[
-                  { id: "1", text: "Support chat", icon: "1" },
-                  { id: "2", text: "Project Page", icon: "2" },
-                  { id: "2", text: "Reset Cache", icon: "2" },
+                  {
+                    id: "1",
+                    text: "Support chat",
+                    icon: <BsSlack />,
+                    onClick: openSupportChat,
+                  } as DropDownItem,
+                  {
+                    id: "2",
+                    text: "Project Page",
+                    icon: <BsGithub />,
+                    onClick: openProjectPage,
+                  },
+                  { id: "3", isSeparator: true },
+                  {
+                    id: "4",
+                    text: "Reset Cache",
+                    icon: <BsArrowRepeat />,
+                    onClick: resetCache,
+                  },
+                  {
+                    id: "5",
+                    text: "REST API",
+                    icon: <BsBraces />,
+                    onClick: openAPI,
+                  },
+                  { id: "6", isSeparator: true },
+                  { id: "7", text: "version 0.0.0", isDisabled: true },
                 ]}
               ></DropDown>
             </li>
@@ -57,7 +114,6 @@ export default function Header() {
         </div>
       </div>
       <div className="h-16 flex items-center gap-5 ">
-        
         <div className="flex p-1 gap-2 border bottom-gray-200 rounded">
           <img src={WatcherIcon} width={40} height={40} />
           <div className="flex flex-col">
@@ -65,14 +121,19 @@ export default function Header() {
               href="https://komodor.com/helm-dash/"
               className="text-[#0d6efd] font-bold"
             >
-              Upgrade your HELM experience - Free
+              <div className="flex items-center gap-2">
+                Upgrade your HELM experience - Free
+                <BsBoxArrowUpRight className="w-[14px] h-[14px]" />
+              </div>
             </a>
-            <label className="text-[#707583]">Auth & RBAC, k8s events, troubleshooting and more</label>
+            <label className="text-[#707583]">
+              Auth & RBAC, k8s events, troubleshooting and more
+            </label>
           </div>
         </div>
 
         <span className="w-[1px] h-3/4 bg-gray-200" />
-        <ShutDownButton/>
+        <ShutDownButton />
       </div>
     </div>
   );
