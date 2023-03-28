043c9ae87faaa4e86715a17eca95900ba2e9d563
Erez Birenholz <biran330@gmail.com>
2023-03-28 23:49:59 +0300

    Add some common types

--- dashboard/src/data/types.ts
+++ dashboard/src/data/types.ts
@@ -4,4 +4,44 @@ export type Revision = {
 
 export type Chart = {
   name: string;
+  home: string;
+  sources: string[];
+  version: string;
+  description: string;
+  keywords: string[];
+  maintainers: {
+    name: string;
+    url: string;
+  }[];
+  icon: string;
+  apiVersion: string;
+  appVersion: string;
+  annotations: {
+    category: string;
+    licenses: string;
+  };
+  dependencies: {
+    name: string;
+    version: string;
+    repository: string;
+    condition?: string;
+    tags?: string[];
+  }[];
+  urls: string[];
+  created: string;
+  digest: string;
+};
+
+export type Release = {
+  name: string;
+  namespace: string;
+  revision: string;
+  updated: string;
+  status: string;
+  chart: string;
+  chartName: string;
+  chartVersion: string;
+  app_version: string;
+  icon: string;
+  description: string;
 };
