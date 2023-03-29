7f51efe618cd21ba6fb24efdf976e940bf961792
yossi <a534158711@gmail.com>
2023-03-29 10:30:21 +0300

    Installed page - installed charts - ui

---
+++ dashboard/src/components/InstalledPackagesArea.tsx/InstalledPackagesList.tsx
@@ -0,0 +1,19 @@
+import { Dispatch, SetStateAction } from "react";
+import { InstalledPackage } from "../../pages/Installed";
+import InstalledPackageCard from "./InstalledPackageCard";
+
+type Props = {
+    installedPackages: InstalledPackage[];
+    setInstalledPackages: Dispatch<SetStateAction< InstalledPackage[]>>;
+}
+
+export default function InstalledPackagesList(props: Props) {
+
+    const {installedPackages} = props;
+
+    return (
+        <div>
+            {installedPackages.map((installedPackage: InstalledPackage) => <InstalledPackageCard installedPackage={installedPackage} />)}
+        </div>
+    )
+}
--- dashboard/src/pages/Installed.tsx
+++ dashboard/src/pages/Installed.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { useState } from 'react';
+import InstalledPackagesHeader from '../components/InstalledPackagesArea.tsx/InstalledPackagesHeader';
+import InstalledPackagesList from '../components/InstalledPackagesArea.tsx/InstalledPackagesList';
+export class InstalledPackage{
+  version: string
+  name: string;
+  revision: number;
+  lastUpdated: string;
+  description: string;
+  constructor(
+    version: string,
+    name: string,
+    revision: number,
+    lastUpdated: string,
+    description: string
+  ){ 
+    this.version = version;
+    this.name = name;
+    this.revision = revision;
+    this.lastUpdated = lastUpdated;
+    this.description = description;
+   };
+}
+const currentPackage = {
+  version: "0.1.0",
+  name: "airflow",
+  revision: 1,
+  lastUpdated: "9d",
+  description: "Apache Airflow is a tool th express and execute workflows as directed acyclic graphs (DAGs) it",
+}
 
 function Installed() {
+
+  const [installedPackages, setInstalledPackages] = useState([currentPackage, currentPackage]);
+
   return (
-    <div>Installed</div>
+    <div
+      className='p-5'
+    >
+      <InstalledPackagesHeader installedPackages={installedPackages} />
+
+      <InstalledPackagesList installedPackages={installedPackages} setInstalledPackages={setInstalledPackages} />
+
+      
+    </div>
   )
 }
 
-export default Installed
+export default Installed;
