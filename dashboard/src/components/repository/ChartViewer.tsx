2e0137ea62e02554f115583a364c20c095df3a1d
Erez Birenholz <biran330@gmail.com>
2023-04-10 14:44:06 +0300

    Open install modal

--- dashboard/src/components/repository/ChartViewer.tsx
+++ dashboard/src/components/repository/ChartViewer.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Chart } from "../../data/types";
+import InstallModal from "../modal/InstallModal";
 
 type ChartViewerProps = {
   chart: Partial<Chart>;
@@ -17,31 +18,38 @@ function ChartViewer({ chart }: ChartViewerProps) {
   };
 
   return (
-    <div
-      className="grid grid-cols-5 hover:bg-[#f4f7fa] p-4"
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-    >
-      <span className="col-span-1 font-semibold flex flex-row items-center gap-1">
-        <img
-          src="https://bitnami.com/assets/stacks/airflow/img/airflow-stack-220x234.png"
-          className="h-4"
-        />
-        {chart.name}
-      </span>
-      <span className="col-span-2 text-sm">{chart.description}</span>
-      <span className="col-span-1">{chart.version}</span>
-      <span className="col-span-1">
-        {showInstallButton && (
-          <button
-            className="bg-white border border-gray-300 p-1"
-            onClick={() => setShowInstallModal(true)}
-          >
-            Install
-          </button>
-        )}
-      </span>
-    </div>
+    <>
+      <div
+        className="grid grid-cols-5 hover:bg-[#f4f7fa] p-4"
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+      >
+        <span className="col-span-1 font-semibold flex flex-row items-center gap-1">
+          <img
+            src="https://bitnami.com/assets/stacks/airflow/img/airflow-stack-220x234.png"
+            className="h-4"
+          />
+          {chart.name}
+        </span>
+        <span className="col-span-2 text-sm">{chart.description}</span>
+        <span className="col-span-1">{chart.version}</span>
+        <span className="col-span-1">
+          {showInstallButton && (
+            <button
+              className="bg-white border border-gray-300 p-1 px-2 rounded-md font-semibold"
+              onClick={() => setShowInstallModal(true)}
+            >
+              Install
+            </button>
+          )}
+        </span>
+      </div>
+      <InstallModal
+        chart={chart}
+        isOpen={showInstallModal}
+        onConfirm={() => setShowInstallModal(false)}
+      />
+    </>
   );
 }
 
--- dashboard/src/components/repository/RepositoriesList.tsx
+++ dashboard/src/components/repository/RepositoriesList.tsx
@@ -10,19 +10,26 @@ function RepositoriesList() {
   return (
     <div className="bg-white flex flex-col p-2 rounded shadow-md text-[#3d4048] w-1/6 m-5 gap-3">
       <label className="font-bold">Repositories</label>
-      {repositories.map((repository) => (
-        <span className="flex items-center" key={repository.id}>
-          <input
-            type="radio"
-            id={repository.id}
-            value={repository.name}
-            name="clusters"
-          />
-          <label className="ml-1">{repository.name}</label>
-        </span>
-      ))}
-      <button type="button"  className="bg-white border border-gray-300 p-1 self-start">+ Add Repository</button>
-      <p className="text-xs">
+      <div className="flex flex-col gap-1">
+        {repositories.map((repository) => (
+          <span className="flex items-center" key={repository.id}>
+            <input
+              type="radio"
+              id={repository.id}
+              value={repository.name}
+              name="clusters"
+            />
+            <label className="ml-1">{repository.name}</label>
+          </span>
+        ))}
+      </div>
+      <button
+        type="button"
+        className="bg-white border border-gray-300 p-1 self-start"
+      >
+        + Add Repository
+      </button>
+      <p className="text-sm">
         Charts developers: you can also add local directories as chart source.
         Use <span className="text-green-600">--local-chart</span> CLI switch to
         specify it.
