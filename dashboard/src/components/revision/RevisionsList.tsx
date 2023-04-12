33a8dfac6d13b39d4293ac6ce19b024079e52992
Erez Birenholz <biran330@gmail.com>
2023-04-12 08:58:48 +0300

    Make the Revisions list selectable

--- dashboard/src/components/revision/RevisionsList.tsx
+++ dashboard/src/components/revision/RevisionsList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { ReleaseRevision } from "../../data/types";
 import { getAge } from "../../timeUtils";
 
@@ -5,19 +6,30 @@ type RevisionsListProps = {
   releaseRevisions: ReleaseRevision[];
 };
 
-export default function RevisionsList({ releaseRevisions }: RevisionsListProps) {
+export default function RevisionsList({
+  releaseRevisions,
+}: RevisionsListProps) {
+  const [selectedRevision, setSelectedRevision] = useState(1);
+
   return (
     <>
       {releaseRevisions.map((revision) => (
         <div
+          onClick={() => setSelectedRevision(revision.revision)}
           key={revision.revision}
-          className="flex flex-col border border-[#007bff] bg-white rounded-md mx-5 p-2 gap-4"
+          className={`flex flex-col border rounded-md mx-5 p-2 gap-4 cursor-pointer ${
+            revision.revision === selectedRevision ? "border-[#007bff] bg-white" : "border-[#DCDDDF] bg-[#F4F7FA]"
+          }`}
         >
           <div className="flex row justify-between">
-            <span className="text-[#1FA470] font-semibold">● {revision.status.toUpperCase()}</span>
+            <span className="text-[#1FA470] font-semibold">
+              ● {revision.status.toUpperCase()}
+            </span>
             <span className="font-semibold">#{revision.revision}</span>
           </div>
-          <div className="self-end text-[#707583] text-xs">AGE:{getAge(revision.updated)}</div>
+          <div className="self-end text-[#707583] text-xs">
+            AGE:{getAge(revision.updated)}
+          </div>
         </div>
       ))}
     </>
