b13d82c2f470c075e65a457c9ebdc5719557e035
Harshit Mehta <hdm23061993@gmail.com>
2023-04-14 12:01:28 +0530

    bug: fix sorting of release versions
    Signed-off-by: Harshit Mehta <hdm23061993@gmail.com>

--- pkg/dashboard/static/actions.js
+++ pkg/dashboard/static/actions.js
@@ -91,7 +91,7 @@ function popUpUpgrade(elm, ns, name, verCur, lastRev) {
         $.getJSON("/api/helm/repositories/versions?name=" + elm.name).fail(function (xhr) {
             reportError("Failed to find chart in repo", xhr)
         }).done(function (vers) {
-            vers.sort((b, a) => (a.version > b.version) - (a.version < b.version))
+            vers = vers.map( a => a.split('.').map( n => +n+100000 ).join('.') ).sort().map( a => a.split('.').map( n => +n-100000 ).join('.') );
 
             // fill versions
             $('#upgradeModal select').empty()
