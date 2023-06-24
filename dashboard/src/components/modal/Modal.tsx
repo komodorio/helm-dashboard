e5a82708c39941e33f1ff294ee216305999104c2
Nir Parisian <nir.parisian@gmail.com>
2023-06-24 22:30:47 +0300

    signout fix

--- dashboard/src/components/modal/Modal.tsx
+++ dashboard/src/components/modal/Modal.tsx
@@ -19,7 +19,7 @@ export interface ModalAction {
 export interface ModalProps extends PropsWithChildren {
   title?: string | ReactNode;
   isOpen: boolean;
-  onClose: () => void;
+  onClose?: () => void;
   containerClassNames?: string;
   actions?: ModalAction[];
   bottomContent?: ReactNode;
@@ -91,25 +91,27 @@ const Modal = ({
               {title && (
                 <div className="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
                   {getTitle(title)}
-                  <button
-                    type="button"
-                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="staticModal"
-                    onClick={onClose}
-                  >
-                    <svg
-                      className="w-5 h-5"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
+                  {onClose ? (
+                    <button
+                      type="button"
+                      className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                      data-modal-hide="staticModal"
+                      onClick={onClose}
                     >
-                      <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                      ></path>
-                    </svg>
-                  </button>
+                      <svg
+                        className="w-5 h-5"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clipRule="evenodd"
+                        ></path>
+                      </svg>
+                    </button>
+                  ) : null}
                 </div>
               )}
               <div
@@ -118,20 +120,20 @@ const Modal = ({
               >
                 {children}
               </div>
-              {bottomContent ??
-              <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600"> 
-                {actions?.map((action) => (
-                  <button
-                    key={action.id}
-                    type="button"
-                    className={getClassName(action)}
-                    onClick={action.callback}
-                  >
-                    {action.text}
-                  </button>
-                ))}
-               </div> 
-                }
+              {bottomContent ?? (
+                <div className="flex justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
+                  {actions?.map((action) => (
+                    <button
+                      key={action.id}
+                      type="button"
+                      className={getClassName(action)}
+                      onClick={action.callback}
+                    >
+                      {action.text}
+                    </button>
+                  ))}
+                </div>
+              )}
             </div>
           </div>
         </div>
