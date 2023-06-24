9fddc1507363e759b8c3e4f974249a7d581a23a6
rotembm12 <rotembm12@gmail.com>
2023-06-24 21:26:19 +0300

    pr

--- dashboard/src/components/revision/RevisionResource.tsx
+++ dashboard/src/components/revision/RevisionResource.tsx
@@ -1,7 +1,13 @@
 import { uniqueId } from "lodash";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { useParams } from "react-router-dom";
 import { StructuredResources, useGetResources } from "../../API/releases";
+
+// import component 👇
+import Drawer from "react-modern-drawer";
+
+//import styles 👇
+import "react-modern-drawer/dist/index.css";
 
 import Button from "../Button";
 import Badge from "../Badge";
@@ -38,6 +44,10 @@ const ResourceRow = ({ resource }: { resource: StructuredResources }) => {
     metadata: { name },
     status: { conditions },
   } = resource;
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleDrawer = () => {
+    setIsOpen((prevState) => !prevState);
+  };
   const { reason = "", status = "" } = conditions?.[0] || {};
   const cellClassnames = "py-2";
   const rowId = useMemo(() => uniqueId(), []);
@@ -56,10 +66,46 @@ const ResourceRow = ({ resource }: { resource: StructuredResources }) => {
         </td>
         <td className={"rounded " + cellClassnames}>
           <div className="flex justify-end items-center pr-4">
-            <Button className="bg-white">Describe</Button>
+            <Button onClick={toggleDrawer} className="bg-white">
+              Describe
+            </Button>
           </div>
         </td>
       </tr>
+      <Drawer
+        open={isOpen}
+        onClose={toggleDrawer}
+        direction="right"
+        className="bla bla bla"
+      >
+        <div className="offcanvas-header border-bottom p-4">
+          <div>
+            <h5 id="describeModalLabel">
+              my-release-mysql
+              <span className="badge me-2 fw-normal bg-success text-dark bg-opacity-50 ms-3 small">
+                Exists
+              </span>
+            </h5>
+            <p className="m-0 mt-4">StatefulSet</p>
+          </div>
+          <div>
+            <a
+              href="https://www.komodor.com/helm-dash/?utm_campaign=Helm%20Dashboard%20%7C%20CTA&amp;utm_source=helm-dash&amp;utm_medium=cta&amp;utm_content=helm-dash"
+              className="btn btn-primary btn-sm me-2"
+              target="_blank"
+            >
+              See more details in Komodor{" "}
+              <i className="bi-box-arrow-up-right"></i>
+            </a>
+            <button
+              type="button"
+              className="btn-close text-reset"
+              data-bs-dismiss="offcanvas"
+              aria-label="Close"
+            ></button>
+          </div>
+        </div>
+      </Drawer>
     </>
   );
 };
