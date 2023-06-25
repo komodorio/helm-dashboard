199caaa658108fd77145acd13943e1c15e6754a6
chad11111 <127234894+chad11111@users.noreply.github.com>
2023-06-25 16:48:18 +0300

    refactor (#386)

---
+++ dashboard/src/context/ErrorModalContext.tsx
@@ -0,0 +1,14 @@
+import React, { createContext } from "react";
+
+export interface ErrorAlert {
+  title?: string,
+  msg: string,
+}
+
+export const ErrorModalContext = createContext<{
+  shouldShowErrorModal?:ErrorAlert
+  setShowErrorModal: (toggle?: ErrorAlert) => void
+}>({
+  shouldShowErrorModal: undefined,
+  setShowErrorModal: (toggle?: ErrorAlert) => {}
+});
