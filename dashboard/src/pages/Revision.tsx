7ce2c54ba779b6acadd26e641b3b27614c9a6874
rotembm12 <rotembm12@gmail.com>
2023-07-03 23:39:06 +0300

    loading ui

--- dashboard/src/pages/Revision.tsx
+++ dashboard/src/pages/Revision.tsx
@@ -5,6 +5,7 @@ import RevisionsList from "../components/revision/RevisionsList";
 import { ReleaseRevision } from "../data/types";
 import { useQuery } from "@tanstack/react-query";
 import apiService from "../API/apiService";
+import Spinner from "../components/Spinner";
 
 const descendingSort = (r1: ReleaseRevision, r2: ReleaseRevision) =>
   r1.revision - r2.revision < 0 ? 1 : -1;
@@ -14,12 +15,15 @@ function Revision() {
 
   const selectedRevision = revision ? parseInt(revision, 10) : 0;
 
-  const { data: releaseRevisions, refetch: refetchRevisions } = useQuery<
-    ReleaseRevision[]
-  >({
+  const {
+    data: releaseRevisions,
+    refetch: refetchRevisions,
+    isLoading: isLoadinHistory,
+  } = useQuery<ReleaseRevision[]>({
     queryKey: ["releasesHistory", restParams],
     queryFn: apiService.getReleasesHistory,
   });
+
   const sortedReleases = useMemo(
     () => releaseRevisions?.sort(descendingSort),
     [releaseRevisions]
@@ -34,32 +38,49 @@ function Revision() {
     return null;
   }, [releaseRevisions, selectedRevision]);
 
-  if (!releaseRevisions) return <></>;
-
   return (
     <div className="flex">
       <div className="flex flex-col gap-2 w-1/6 min-h-screen bg-[#E8EDF2] pb-4">
         <label className="mt-5 mx-5 text-sm text-[#3D4048] font-semibold">
           Revisions
         </label>
-        <RevisionsList
-          releaseRevisions={sortedReleases}
-          selectedRevision={selectedRevision}
-        />
+        {isLoadinHistory ? (
+          <RevisionSidebarSkeleton />
+        ) : (
+          <RevisionsList
+            releaseRevisions={sortedReleases}
+            selectedRevision={selectedRevision}
+          />
+        )}
       </div>
 
       <div className="w-5/6 min-h-screen bg-[#F4F7FA] pb-4">
-        {selectedRelease ? (
+        {isLoadinHistory ? (
+          <div className=" p-4">
+            <Spinner />
+          </div>
+        ) : selectedRelease ? (
           <RevisionDetails
             release={selectedRelease}
             refetchRevisions={refetchRevisions}
           />
-        ) : (
-          ""
-        )}
+        ) : null}
       </div>
     </div>
   );
 }
+
+const RevisionSidebarSkeleton = () => {
+  return (
+    <>
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+      <div className="border rounded-md mx-5 p-2 gap-4 animate-pulse  h-[80px] w-[88%] bg-gray-100" />
+    </>
+  );
+};
 
 export default Revision;
