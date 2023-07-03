f8b685b5ad25b90d7cce378b2bc90c4874cb5e69
rotembm12 <rotembm12@gmail.com>
2023-07-03 22:13:56 +0300

    Fixed vroken jsx

--- dashboard/src/components/revision/RevisionsList.tsx
+++ dashboard/src/components/revision/RevisionsList.tsx
@@ -1,6 +1,6 @@
 import { BsArrowDownLeft, BsArrowUpRight } from "react-icons/bs";
-import {  useParams } from "react-router-dom";
-import { compare } from 'compare-versions';
+import { useParams } from "react-router-dom";
+import { compare } from "compare-versions";
 
 import { ReleaseRevision } from "../../data/types";
 import { getAge } from "../../timeUtils";
@@ -19,7 +19,9 @@ export default function RevisionsList({
   const navigate = useNavigateWithSearchParams();
   const { context, namespace, chart } = useParams();
   const changeRelease = (newRevision: number) => {
-    navigate(`/installed/revision/${context}/${namespace}/${chart}/${newRevision}`);
+    navigate(
+      `/installed/revision/${context}/${namespace}/${chart}/${newRevision}`
+    );
   };
 
   return (
@@ -41,28 +43,47 @@ export default function RevisionsList({
             }`}
           >
             <div className="flex row justify-between">
-              <StatusLabel status={release.status} isRollback={release.description.startsWith("Rollback to ")}/>
+              <StatusLabel
+                status={release.status}
+                isRollback={release.description.startsWith("Rollback to ")}
+              />
               <span className="font-semibold">#{release.revision}</span>
             </div>
-            <div className="self-end text-[#707583] text-xs flex flex-wrap gap-1" style={{
-              width: "100%",
-              display: "flex",
-              justifyContent: "space-between",
-            }}>
-              <span style={{
+            <div
+              className="self-end text-[#707583] text-xs flex flex-wrap gap-1"
+              style={{
+                width: "100%",
                 display: "flex",
-                width: "70px",
                 justifyContent: "space-between",
-              }}>
-              {prevRelease  ? (
-                (compare(prevRelease.chart_ver, release.chart_ver, '!=') && <>
-                  <span className="line-through">{prevRelease.chart_ver}</span>
-                  {compare(prevRelease.chart_ver, release.chart_ver, '>') ? <BsArrowDownLeft /> : <BsArrowUpRight />}
-                  <span>{release.chart_ver}</span>
-                </>)
-              ) : (
-                ""
-              )}
+              }}
+            >
+              <span
+                style={{
+                  display: "flex",
+                  width: "70px",
+                  justifyContent: "space-between",
+                }}
+              >
+                {prevRelease
+                  ? compare(prevRelease.chart_ver, release.chart_ver, "!=") && (
+                      <>
+                        <span className="line-through">
+                          {prevRelease.chart_ver}
+                        </span>
+                        {compare(
+                          prevRelease.chart_ver,
+                          release.chart_ver,
+                          ">"
+                        ) ? (
+                          <BsArrowDownLeft />
+                        ) : (
+                          <BsArrowUpRight />
+                        )}
+                        <span>{release.chart_ver}</span>
+                      </>
+                    )
+                  : ""}
+              </span>
               <span>AGE:{getAge(release, releaseRevisions[idx - 1])}</span>
             </div>
           </div>
