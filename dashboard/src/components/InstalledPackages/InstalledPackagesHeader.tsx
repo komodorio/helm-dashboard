79fdef72db278f5d3809f3203ae04aae7c87d1e1
Nir Parisian <nir.parisian@gmail.com>
2023-07-05 23:13:24 +0300

    more rounding

--- dashboard/src/components/InstalledPackages/InstalledPackagesHeader.tsx
+++ dashboard/src/components/InstalledPackages/InstalledPackagesHeader.tsx
@@ -32,7 +32,7 @@ export default function InstalledPackagesHeader({
 
         <div className="w-1/3">
           <input
-            className="border border-[#ced4da] rounded-sm p-1 text-sm w-11/12"
+            className="border border-[#ced4da] rounded p-1 text-sm w-11/12"
             placeholder="Filter..."
             type="text"
             onChange={(ev) => setFilterKey(ev.target.value)}
