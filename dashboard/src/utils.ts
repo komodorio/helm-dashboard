ce20ea7efcfceb712e137aa9767cc5706fa57588
Nir Parisian <nir.parisian@gmail.com>
2023-07-11 20:54:15 +0300

    tag should not be visible

--- dashboard/src/utils.ts
+++ dashboard/src/utils.ts
@@ -37,5 +37,9 @@ export const diffConfiguration: Diff2HtmlUIConfig = {
     "file-summary-wrapper": '<div class="hidden"></div>', // hide this element
     "generic-line":
       '<tr><td class="{{lineClass}} {{type}}">{{{lineNumber}}}</td><td class="{{type}}"><div class="{{contentClass}} w-auto">{{#prefix}}<span class="d2h-code-line-prefix">{{{prefix}}}</span>{{/prefix}}{{^prefix}}<span class="d2h-code-line-prefix">&nbsp;</span>{{/prefix}}{{#content}}<span class="d2h-code-line-ctn">{{{content}}}</span>{{/content}}{{^content}}<span class="d2h-code-line-ctn"><br></span>{{/content}}</div></td></tr>', // added "w-auto" to most outer div to prevent horizontal scroll
+    "tag-file-changed": "",
+    "tag-file-renamed": "",
+    "tag-file-added": "",
+    "tag-file-deleted": "",
   },
 };
