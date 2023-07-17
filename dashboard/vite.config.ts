678a37d38d2c2bcb45acb55b3de9d56ccfe8f459
chad11111 <chad1111@tutanota.com>
2023-07-17 13:17:14 -0400

    refactor

--- dashboard/vite.config.ts
+++ dashboard/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     build: {
-      assetsDir: "./assets",
+      assetsDir: "./assets/",
       outDir: "../pkg/frontend/dist",
       emptyOutDir: true,
     },
