02b9c23efd88276042bd346f9876d875be4edfae
Tamir Abutbul <1tamir198@gmail.com>
2023-08-02 23:56:57 +0300

    Extract stories out of stories folder

--- dashboard/src/components/common/Header/Header.stories.tsx
+++ dashboard/src/components/common/Header/Header.stories.tsx
@@ -1,5 +1,4 @@
-import React from "react"
-import { ComponentStory, ComponentMeta } from "@storybook/react"
+import { ComponentMeta, ComponentStory } from "@storybook/react"
 
 import { Header } from "./Header"
 
