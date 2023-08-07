7f89fedaa23ceed43b41c36e39d24c8890092335
Tamir Abutbul <1tamir198@gmail.com>
2023-08-07 18:34:57 +0300

    Refactor stories fodler (#450)

--- dashboard/src/components/common/Header/Header.tsx
+++ dashboard/src/components/common/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Button } from "./Button";
+import { Button } from "../Button/Button";
 import "./header.css";
 
 type User = {
--- dashboard/src/components/common/Page/Page.tsx
+++ dashboard/src/components/common/Page/Page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Header } from "./Header";
+import { Header } from "../Header/Header";
 import "./page.css";
 
 type User = {
