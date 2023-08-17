5b85145804ae9832cf121a546e31bcc47c5a9da2
Nir Parisian <nir2002@users.noreply.github.com>
2023-08-17 11:34:36 +0300

    Update dashboard/src/pages/DocsPage.tsx

--- dashboard/src/pages/DocsPage.tsx
+++ dashboard/src/pages/DocsPage.tsx
@@ -1,6 +1,6 @@
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import openapi from '../../public/openapi.json';
+import openapi from "../../public/openapi.json";
 
 const DocsPage = () => {
   return <SwaggerUI spec={openapi} />;
