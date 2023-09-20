78fb0d2f6bb106ffe2b530fc93e49539bcbf62c8
Andrey Pokhilko <andrei@komodor.io>
2023-09-20 11:39:01 +0100

    Update analytics.js with UUID fallback

--- dashboard/public/analytics.js
+++ dashboard/public/analytics.js
@@ -153,8 +153,15 @@ const getUserId = (() => {
   let userId = null;
   return () => {
     if (!userId) {
-      userId = crypto.randomUUID();
+      userId = crypto.randomUUID?crypto.randomUUID():uuid();
     }
     return userId;
   };
 })();
+
+function uuid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
