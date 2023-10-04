a2dd26114a5a3c32a29dda2bb4e2e749a307c032
Tamir Abutbul <1tamir198@gmail.com>
2023-10-04 23:49:42 +0300

    Adding cypress config

---
+++ frontend/cypress/support/component.ts
@@ -0,0 +1,12 @@
+import "./commands";
+import { mount } from "cypress/react18";
+
+declare global {
+  namespace Cypress {
+    interface Chainable {
+      mount: typeof mount;
+    }
+  }
+}
+
+Cypress.Commands.add("mount", mount);
