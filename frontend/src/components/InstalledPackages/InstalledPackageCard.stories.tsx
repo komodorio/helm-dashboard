bb66c5de5bd96905135b5e79c963a0b281467a87
kamegoro <tentyo0620@icloud.com>
2023-11-02 02:05:59 +0900

    Migrate InstalledPackages directory to CSF3 notation

--- frontend/src/components/InstalledPackages/InstalledPackageCard.stories.tsx
+++ frontend/src/components/InstalledPackages/InstalledPackageCard.stories.tsx
@@ -1,41 +1,35 @@
-// InstalledPackageCard.stories.ts|tsx
-
-import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Meta } from "@storybook/react";
 import InstalledPackageCard from "./InstalledPackageCard";
 
-//👇 This default export determines where your story goes in the story list
-export default {
+const meta = {
   /* 👇 The title prop is optional.
    * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
    * to learn how to generate automatic titles
    */
   title: "InstalledPackageCard",
   component: InstalledPackageCard,
-} as ComponentMeta<typeof InstalledPackageCard>;
+} satisfies Meta<typeof InstalledPackageCard>;
 
-//👇 We create a “template” of how args map to rendering
-const Template: ComponentStory<typeof InstalledPackageCard> = (args) => (
-  <InstalledPackageCard {...args} />
-);
+export default meta;
 
-export const Default = Template.bind({});
-
-Default.args = {
-  release: {
-    id: "",
-    name: "",
-    namespace: "",
-    revision: 1,
-    updated: "",
-    status: "",
-    chart: "",
-    chart_name: "",
-    chart_ver: "",
-    app_version: "",
-    icon: "",
-    description: "",
-    has_tests: false,
-    chartName: "", // duplicated in some cases in the backend, we need to resolve this
-    chartVersion: "", // duplicated in some cases in the backend, we need to resolve this
+export const Default = {
+  args: {
+    release: {
+      id: "",
+      name: "",
+      namespace: "",
+      revision: 1,
+      updated: "",
+      status: "",
+      chart: "",
+      chart_name: "",
+      chart_ver: "",
+      app_version: "",
+      icon: "",
+      description: "",
+      has_tests: false,
+      chartName: "", // duplicated in some cases in the backend, we need to resolve this
+      chartVersion: "", // duplicated in some cases in the backend, we need to resolve this
+    },
   },
 };
