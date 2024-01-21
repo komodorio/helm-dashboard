520d9519015a1d1d4770ef73ab04d1b6b67eaf96
Tamir Abutbul <1tamir198@gmail.com>
2024-01-21 22:09:13 +0200

    Cleaning the test

--- frontend/cypress/e2e/addRepository.cy.ts
+++ frontend/cypress/e2e/addRepository.cy.ts
@@ -1,8 +1,16 @@
 describe("Adding repository flow", () => {
+  const addChartNameInput = "[data-cy='add-chart-name']";
+  const addChartUrlInput = "[data-cy='add-chart-url']";
+  const addChartRepositoryButton = "[data-cy='add-chart-repository-button']";
+
   it("Adding new chart repository", () => {
     cy.intercept("GET", "http://localhost:5173/status", {
       fixture: "status.json",
     }).as("status");
+
+    cy.intercept("GET", "http://localhost:5173/api/helm/releases", {
+      fixture: "releases.json",
+    }).as("releases");
 
     cy.visit(
       "http://localhost:5173/#/minikube/installed?filteredNamespace=default"
@@ -11,14 +19,14 @@ describe("Adding repository flow", () => {
     cy.get("[data-cy='navigation-link']").contains("Repository").click();
     cy.get("[data-cy='install-repository-button']").click();
 
-    cy.get("[data-cy='add-chart-name']").type("Komodorio");
-    cy.get("[data-cy='add-chart-url']").type("https://helm-charts.komodor.io");
+    cy.get(addChartNameInput).type("Komodorio");
+    cy.get(addChartUrlInput).type("https://helm-charts.komodor.io");
 
     cy.intercept("GET", "http://localhost:5173/api/helm/repositories", {
       fixture: "repositories.json",
     }).as("repositories");
 
-    cy.get("[data-cy='add-chart-repository-button']").click();
+    cy.get(addChartRepositoryButton).click();
 
     cy.contains("https://helm-charts.komodor.io");
 
@@ -40,8 +48,6 @@ describe("Adding repository flow", () => {
 
     cy.contains("Confirm").click();
 
-    cy.wait("@defaultReleases");
-
-    cy.wait("@history");
+    cy.wait(["@defaultReleases", "@history"]);
   });
 });
