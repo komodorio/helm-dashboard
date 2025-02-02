5b2f1e2818fea12e4f13f20798febd448573a922
Andrei Pohilko <andrei.pokhilko@gmail.com>
2025-02-02 21:51:17 +0000

    Fix trailing space

--- ci/bump-versions.sh
+++ ci/bump-versions.sh
@@ -8,8 +8,8 @@ WORKING_DIRECTORY="$PWD"
   APP_VERSION=$(cat ${HELM_CHARTS_SOURCE}/Chart.yaml | grep 'appVersion:' | awk -F'"' '{print $2}')
 }
 
-sed -i -e "s/appVersion.*/appVersion: \"${APP_VERSION}\" /g" ${HELM_CHARTS_SOURCE}/Chart.yaml
-sed -i -e "s/version.*/version: \"${APP_VERSION}\" /g" plugin.yaml
+sed -i -e "s/appVersion.*/appVersion: \"${APP_VERSION}\"/g" ${HELM_CHARTS_SOURCE}/Chart.yaml
+sed -i -e "s/version.*/version: \"${APP_VERSION}\"/g" plugin.yaml
 CURRENT_VERSION=$(cat ${HELM_CHARTS_SOURCE}/Chart.yaml | grep 'version:' | awk '{print $2}')
 NEW_VERSION=$(echo $CURRENT_VERSION | awk -F. '{$NF = $NF + 1;} 1' | sed 's/ /./g')
 sed -i -e "s/${CURRENT_VERSION}/${NEW_VERSION}/g" ${HELM_CHARTS_SOURCE}/Chart.yaml
