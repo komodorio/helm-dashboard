18951f9222cd2aa5324e4c58cf7a13ce749fec79
yuri-sakharov <10849682+yuri-sakharov@users.noreply.github.com>
2025-11-27 23:17:55 +0200

    Fix husky

--- frontend/src/API/k8s.ts
+++ frontend/src/API/k8s.ts
@@ -5,8 +5,11 @@ import apiService from "./apiService";
 
 // Get list of kubectl contexts configured locally
 function useGetKubectlContexts(options?: UseQueryOptions<KubectlContexts>) {
-  return useQuery<KubectlContexts >({ queryKey: ["k8s", "contexts"],queryFn:() =>apiService.fetchWithDefaults<KubectlContexts>("/api/k8s/contexts"),
-    ...(options ??{}),
+  return useQuery<KubectlContexts>({
+    queryKey: ["k8s", "contexts"],
+    queryFn: () =>
+      apiService.fetchWithDefaults<KubectlContexts>("/api/k8s/contexts"),
+    ...(options ?? {}),
   });
 }
 
