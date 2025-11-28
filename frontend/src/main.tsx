fd1d47dc57946065a8f3cb6dd5703398eeb4fee2
yuri-sakharov <10849682+yuri-sakharov@users.noreply.github.com>
2025-11-28 03:16:20 +0200

    Added ThemeInit

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeInit } from "../.flowbite-react/init";
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <ThemeInit />
     <App />
   </StrictMode>
 );
